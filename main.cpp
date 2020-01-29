ad209f2a1ef5eade8501694923bbb00f4056d529
Charles Schiermeyer <matkatmusic@gmail.com>
2020-01-29 11:25:56 -0800

    Added Part1 Instructions

--- main.cpp
+++ main.cpp
@@ -1,7 +1,127 @@
-#include <iostream>
+#if false
+/*
+ PFM::C++ For Musicians Task
+ Project 1 - Part 1 / 1
+ Video: Chapter 2 Part 1
+ 
+Create a branch named Part1
 
+ write out 10 nouns.
+ for each of the 10 nouns, write out 3 actions it might perform, in plain english.
+ write out how you'd call that action in pseudo code, in the space between each block comment
+ 
+ If the action requires multiple words, use camelCaseToNameIt
+ don't forget the semi-colon after each statement
+ 
+ When you finish, commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
+
+ Send me the the link to your repl.it in a DM on Slack
+
+ Wait for my code review.
+ */
+
+/*
+ example)
+ Noun:       arm
+ action 1:   the arm extends
+ action 2:   the arm flexes
+ action 3:   the arm rotates conter-clockwise
+ */
+arm.extend();
+arm.flex();
+arm.rotateCounterClockwise(); //demonstrates CamelCase
+
+/*
+ 1)
+ Noun:
+ action 1:
+ action 2:
+ action 3:
+ */
+
+
+/*
+ 2)
+ Noun:
+ action 1:
+ action 2:
+ action 3:
+ */
+
+/*
+ 3)
+ Noun:
+ action 1:
+ action 2:
+ action 3:
+ */
+
+
+/*
+ 4)
+ Noun:
+ action 1:
+ action 2:
+ action 3:
+ */
+
+/*
+ 5)
+ Noun:
+ action 1:
+ action 2:
+ action 3:
+ */
+
+
+/*
+ 6)
+ Noun:
+ action 1:
+ action 2:
+ action 3:
+ */
+
+/*
+ 7)
+ Noun:
+ action 1:
+ action 2:
+ action 3:
+ */
+
+
+/*
+ 8)
+ Noun:
+ action 1:
+ action 2:
+ action 3:
+ */
+
+/*
+ 9)
+ Noun:
+ action 1:
+ action 2:
+ action 3:
+ */
+
+
+/*
+ 10)
+ Noun:
+ action 1:
+ action 2:
+ action 3:
+ */
+
+
+#endif
+
+#include <iostream>
 int main()
 {
-    std::cout << "good to go!" << std::endl;
-    return 0;    
+    std::cout << "good to go" << std::endl;
+    return 0;
 }
