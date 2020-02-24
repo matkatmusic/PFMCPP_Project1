36854c310da806b0355a18aea06d06c5c71fae5b
ThomasBNL <Thomasburgers@gmail.com>
2020-02-24 07:05:04 +0000

    Written out 10 nouns + actions

--- main.cpp
+++ main.cpp
@@ -35,89 +35,124 @@ arm.rotateCounterClockwise(); //demonstrates CamelCase
 
 /*
  1)
- Noun:
- action 1:
- action 2:
- action 3:
+ Noun: bow
+ action 1: string the bow
+ action 2: insert an arrow in the bow
+ action 3: fire an arrow
  */
+
+ bow.string();
+ bow.insertArrow();
+ bow.fireArrow();
 
 
 /*
  2)
- Noun:
- action 1:
- action 2:
- action 3:
+ Noun: shower
+ action 1: turn on the shower
+ action 2: change the shower temperature 
+ action 3: shampoo you hair underneath the shower
  */
+
+ shower.on();
+ shower.changeTemperature();
+ shower.shampooHair();
 
 /*
  3)
- Noun:
- action 1:
- action 2:
- action 3:
+ Noun: potato
+ action 1: boil the potato
+ action 2: slice the potato
+ action 3: eat the potato
  */
 
+potato.boil();
+potato.slice();
+potato.eat();
 
 /*
  4)
- Noun:
- action 1:
- action 2:
- action 3:
+ Noun: console
+ action 1: insert a videogame into the console
+ action 2: start the videogame on the console
+ action 3: finish the game on the console
  */
+
+ console.insertVideogame();
+ console.startVideogame();
+ console.finishVideogame();
 
 /*
  5)
- Noun:
- action 1:
- action 2:
- action 3:
+ Noun: singer
+ action 1: the singer takes some water
+ action 2: the singer warms up the voice
+ action 3: the singer start singing
  */
 
+ singer.drinkWater();
+ singer.warmingupVoice();
+ singer.startSinging();
 
 /*
  6)
- Noun:
- action 1:
- action 2:
- action 3:
+ Noun: elevator
+ action 1: call the elevator
+ action 2: select a destination in the elevator
+ action 3: push the emergency button in the elevator
  */
+
+ elevator.call();
+ elevator.selectDestination();
+ elevator.pushEmergencyButton();
 
 /*
  7)
- Noun:
- action 1:
- action 2:
- action 3:
+ Noun: agenda
+ action 1: schedule a date in the agenda
+ action 2: find the next appointment in the agenda
+ action 3: search for my birthday in the agenda
  */
 
+ agenda.scheduleDate();
+ agenda.findNextAppointment();
+ agenda.searchMyBirthday();
 
 /*
  8)
- Noun:
- action 1:
- action 2:
- action 3:
+ Noun: television
+ action 1: turn on television
+ action 2: switch television channel
+ action 3: increase the volume
  */
+
+ television.on();
+ television.switchChannel();
+ television.increaseVolume();
 
 /*
  9)
- Noun:
- action 1:
- action 2:
- action 3:
+ Noun: baby
+ action 1: change the diaper of the baby
+ action 2: cuddle the baby
+ action 3: feed the baby 
  */
 
+baby.changeDiaper();
+baby.cuddle();
+baby.feed();
 
 /*
  10)
- Noun:
- action 1:
- action 2:
- action 3:
+ Noun: laundry
+ action 1: sort the laundry
+ action 2: put the laundry in the washing machine
+ action 3: hang the clean laundry
  */
 
+ laundry.sort();
+ laundry.insertIntoWashingMachine();
+ laundry.hang();
 
 #endif
 
