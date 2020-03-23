19b392244005bdc2872b492d8d54439e696d0b48
Heedong Kim <welivehere.hee@gmail.com>
2020-03-23 02:58:45 +0000

    Done part 1 assignment

--- main.cpp
+++ main.cpp
@@ -56,87 +56,95 @@ arm.flex();                             // 3)
 //  action 3:   the arm rotates conter-clockwise
 arm.rotateCounterClockwise();           // 4) demonstrates CamelCase
 
-
 //  1)
-//  Noun:
-//  action 1:
+//  Noun: player
+//  action 1: the player attacks
+player.attack();
+//  action 2: the player depends
+player.depends();
+//  action 3: the player moves
+player.moves();
 
-//  action 2:
-
-//  action 3:
- 
 //  2)
-//  Noun:
-//  action 1:
+//  Noun: dog
+//  action 1: the dog eats
+dog.eat()
+//  action 2: the dog dig
+dog.dig();
+//  action 3: the dog bark
+dog.bark();
 
-//  action 2:
-
-//  action 3:
- 
 //  3)
-//  Noun:
-//  action 1:
+//  Noun: student
+//  action 1: the student studies
+student.study();
+//  action 2: the student plays
+student.play();
+//  action 3: the student take a nap
+student.takeANap();
 
-//  action 2:
-
-//  action 3:
- 
 //  4)
-//  Noun:
-//  action 1:
+//  Noun: user
+//  action 1: the user complains
+user.complain();
+//  action 2: the user log in
+user.logOut();
+//  action 3: the user log out
+user.logIn(); 
 
-//  action 2:
-
-//  action 3:
- 
 //  5)
-//  Noun:
-//  action 1:
+//  Noun: area
+//  action 1 : the area is unlocked
+area.unlocked();
+//  action 2: the area is contaminated
+area.contaminated();
+//  action 3: the area is used;
+area.used();
 
-//  action 2:
-
-//  action 3:
- 
 //  6)
-//  Noun:
-//  action 1:
+//  Noun: data
+//  action 1: the data is saved
+data.saved();
+//  action 2: the data is deleted
+data.deleted();
+//  action 3: the data is overwrited
+data.overwrited();
 
-//  action 2:
-
-//  action 3:
- 
 //  7)
-//  Noun:
-//  action 1:
+//  Noun: baby 
+//  action 1: the baby cried
+baby.cry();
+//  action 2: the baby walks
+baby.walk();
+//  action 3: the baby eats
+baby.eat();
 
-//  action 2:
-
-//  action 3:
- 
 //  8)
-//  Noun:
-//  action 1:
+//  Noun: musician
+//  action 1: the musician plays a gutar
+musician.playGuiatr();
+//  action 2:the musician composes
+musician.compose();
+//  action 3: the musician writes a note
+musician.writeNote();
 
-//  action 2:
-
-//  action 3:
- 
 //  9)
-//  Noun:
-//  action 1:
+//  Noun: moblie phone
+//  action 1: the mobile phone was broken
+mobilePhone.brokend();
+//  action 2: the mobile phone was runned battery out
+mobilePhone.runnedOut();
+//  action 3: the moblie phone was launced
+mobliePhone.launched();
 
-//  action 2:
-
-//  action 3:
- 
 //  10)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
- 
+//  Noun: car
+//  action 1: car was crashed
+car.carshed();
+//  action 2: car was slipped
+car.slipped();
+//  action 3: car was washed
+car.washed();
 
 
 #endif
