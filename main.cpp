b416fbfd2f9edc59aabd983e18fa825995460dc6
NiallOBrienBir1 <mikutatz@gmail.com>
2020-06-02 22:25:24 +0000

    Came up with 10 nouns each with 3 associated actions

--- main.cpp
+++ main.cpp
@@ -52,85 +52,94 @@ arm.rotateCounterClockwise();           // 4) demonstrates CamelCase
 
 
 //  1)
-//  Noun:
-//  action 1:
+//  Noun: 		phone 
+//  action 1:	the phone calls another phone
+phone.call();
+//  action 2:	the phone connects to a network
+	phone.connect();
+//  action 3: 	the phone vibrates
+	phone.vibrate();
 
-//  action 2:
-
-//  action 3:
- 
 //  2)
-//  Noun:
-//  action 1:
+//  Noun:		wampler
+//  action 1:	the sampler incoming audio signal to a buffer (foo)
+sampler.record(foo);
+//  action 2:	the sampler playsback an audio file
+sampler.playback(foo);
+//  action 3:	the sampler alters at playerback speed of a sample 
+sampler.speed(foo);
 
-//  action 2:
-
-//  action 3:
- 
 //  3)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
+//  Noun: 		cat 
+//  action 1:	The cat meows
+cat.meow();
+//  action 2:	the cat purs
+cat.pur();
+//  action 3:	the cat sleeps
+cat.sleep();
  
 //  4)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
+//  Noun:		plant 
+//  action 1:	the plant grows
+plant.grow();
+//  action 2:	the plant photpsynthesizes 
+plant.photosyntesize();
+//  action 3:	the plant wilters
+plane.wilter();
  
 //  5)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
+//  Noun:		legion
+//  action 1:	the legion marches to a location foo
+legion.march(foo);
+//  action 2:	the legion encamps for the night
+legion.camp();
+//  action 3:	the legion position into maniples
+legion.postion();
  
-//  6)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
- 
+//  6)	
+//  Noun		multiFX:
+//  action 1:	The unit processing the input with a chorus effect
+multiFX.chorus();
+//  action 2:	The unit processing the input with a bitreduction effect
+multiFX.bitReduction();
+//  action 3:	the effects unit outputs a signal
+multiFX.output();
+	
 //  7)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
+//  Noun:		RGB light
+//  action 1:	set the brightness of the red light  by a integer 0 - 255
+light.setRed(122);
+//  action 2:	set the brightness of the green light  by a integer 0 - 255
+light.setGreen(22);
+//  action 3:	set the brightness of the blue light  by a integer 0 - 255
+light.setBlue(200);
  
-//  8)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
+//  8)		
+//  Noun:		cell	
+//  action 1:	the cell performs mitosis 
+cell.divide();
+//  action 2:	the cell moves its flangelum 
+cell.activeTransport();
+//  action 3:	the cell processes glucose
+cell.metabolize();
  
 //  9)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
+//  Noun:		CD drive	
+//  action 1:	the CD drive reads data
+cdDrive.read();
+//  action 2:	the CD drive writes data
+cdDrive.write();
+//  action 3:	the drive opens the disk tray
+cdDrive.open();
  
 //  10)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
- 
+//  Noun:		A totally non branded smart speaker
+//  action 1:	set an alarm
+alecksa.setalarm();
+//  action 2:	play a song
+alecksa.play();
+//  action 3:	pause the playback of audio
+alecksa.pause();
 
 
 #endif
