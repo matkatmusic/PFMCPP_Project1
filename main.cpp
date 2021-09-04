f68771272c3cbae938e4e2f2834347b85d3511bc
Fred <wictorsson@gmail.com>
2021-09-04 11:36:18 +0000

    Nothing new

--- main.cpp
+++ main.cpp
@@ -132,19 +132,18 @@ synth.simulate();
 //  action 3:Consumes energy
 synth.consumeEnergy();
 
- 
-
-
 #endif
 
 /*
  MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
 
- Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
- 
- If you didn't already: 
-    Make a pull request after you make your first commit
-    pin the pull request link and this repl.it link to our DM thread in a single message.
+ Commit your changes by clicking on the Source Control panel on the left,
+ entering a message, and click [Commit and push].
+ 
+ If you didn't already:
+	Make a pull request after you make your first commit
+	pin the pull request link and this repl.it link to our DM thread in a single
+ message.
 
  send me a DM to review your pull request when the project is ready for review.
 
@@ -152,8 +151,7 @@ synth.consumeEnergy();
  */
 
 #include <iostream>
-int main()
-{
-    std::cout << "good to go" << std::endl;
-    return 0;
+int main() {
+	std::cout << "good to go" << std::endl;
+	return 0;
 }
