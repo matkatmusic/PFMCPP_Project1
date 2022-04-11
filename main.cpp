2985974809469bf9219b8e1306753951792ff04b
Chai <acdoge.cao@gmail.com>
2022-04-11 15:02:16 +0000

    add 10 nouns with 3 actions each might perform

--- main.cpp
+++ main.cpp
@@ -52,85 +52,85 @@ arm.rotateCounterClockwise();           // 4) demonstrates camelCase
 
 
 //  1)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
-
+//  Noun:        system
+//  action 1:    the system upgrades
+system.upgrade();
+//  action 2:    the system downgrades
+system.downgrade();
+//  action 3:    the system collects error messages
+system.collectErrorMessage();
 //  2)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
-
+//  Noun:        dog
+//  action 1:    the dog bites
+dog.bite();
+//  action 2:    the dog runs
+dog.run();
+//  action 3:    the dog barks
+dog.bark();
 //  3)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
-
+//  Noun:        plane
+//  action 1:    the plane takes off
+plane.takeOff();
+//  action 2:    the plane lands
+plane.land();
+//  action 3:    the plane flys
+plane.fly();
 //  4)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
-
+//  Noun:        ball
+//  action 1:    the ball bounces
+ball.bounce();
+//  action 2:    the ball rolls down
+ball.rollDown();
+//  action 3:    the ball floats on water
+ball.floatOnWater();
 //  5)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
-
+//  Noun:         video
+//  action 1:     the video plays
+video.play();
+//  action 2:     the video pauses
+video.pause();
+//  action 3:     the video repeats n times
+video.repeat(n);
 //  6)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
-
+//  Noun:         image
+//  action 1:     the image zooms in
+image.zoomIn();
+//  action 2:     the image zooms out
+image.zoomOut();
+//  action 3:     the image resizes
+image.resize();
 //  7)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
-
+//  Noun:         plugin
+//  action 1:     the plugin activates
+plugin.activate();
+//  action 2:     the plugin deactivates
+plugin.deactivate();
+//  action 3:     the plugin installs
+plugin.install();
 //  8)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
-
+//  Noun:         player
+//  action 1:     the player dribbles
+player.dribble();
+//  action 2:     the player passes the ball
+player.pass();
+//  action 3:     the player shots
+player.shot();
 //  9)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
-
+//  Noun:         file
+//  action 1:     the file saves
+file.save();
+//  action 2:     the file loads
+file.load();
+//  action 3:     the file copys to specific path
+file.copyTo();
 //  10)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
-
+//  Noun:         stack
+//  action 1:     the stack pops
+stack.pop();
+//  action 2:     the stack pushes
+stack.push();
+//  action 3:     the stack clears
+stack.clear();
 
 
 #endif
