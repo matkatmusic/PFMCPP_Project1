aa1833defc65da7323fdcef16df5a85cf0360a65
BlueMarlinAudio <126108550+BlueMarlinAudio@users.noreply.github.com>
2023-02-26 10:42:41 +0000

    Finished the assignment of part1

--- main.cpp
+++ main.cpp
@@ -52,84 +52,103 @@ arm.rotateCounterClockwise();           // 4) demonstrates camelCase
 
 
 //  1)
-//  Noun:
-//  action 1:
+//  Noun: bike
+//  action 1: the bike moves fast 
+bike.moveFast();
+//  action 2: the bike moves slowly
+bike.moveSlow();
+//  action 3: the bike is not moving
+bike.moveNot()
 
-//  action 2:
-
-//  action 3:
-
-//  2)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
+    //  2)
+//  Noun: bird
+//  action 1: bird eat
+bird.eat();
+//  action 2: bird fly
+bird.fly();
+//  action 3: bird whistles
+bird.whistles();
 
 //  3)
-//  Noun:
-//  action 1:
+//  Noun: synth
+//  action 1: the synth makes soft sound
+synth.soundSoft();
+//  action 2: the synth makes sharp sound
+synth.soundSharp();
+//  action 3:the synth makes no sound
+synth.soundNot();
 
-//  action 2:
-
-//  action 3:
+//  3B)
+//  Noun: synth
+//  action 1: the synth makes soft sound, soft = 1;
+synth.sound1();
+//  action 2: the synth makes sharp sound, sharp = 2;
+synth.sound2();
+//  action 3:the synth makes no sound, no sound = 0;
+synth.sound0();
 
 //  4)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
+//  Noun: baby
+//  action 1: the baby sleeps
+baby.sleeps();
+//  action 2: the baby eats
+baby.eats();
+//  action 3: the baby cries    
+baby.cries();
 
 //  5)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
+//  Noun: serve 
+//  action 1: The serve was valid 
+serve.valid();
+//  action 2: The serve was invalid 
+serve.invalid();
+//  action 3: The serve was a serve-ess 
+serve.serve-ess();
 
 //  6)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
+//  Noun: medicine
+//  action 1: the medicine had a positive effect
+medicine.effectPoistive();
+//  action 2: the medicine had a negative effect
+medicine.effectNegative();
+//  action 3: the medicine had no effect
+medicine.effectNo_effect();
 
 //  7)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
+//  Noun: table
+//  action 1: the table was filled
+table.filled();
+//  action 2: the table was empty
+table.empty();
+//  action 3: the table was upside-down
+table.upsideDown();
 
 //  8)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
+//  Noun: steak
+//  action 1: the steak was raw
+steak.raw();
+//  action 2: the steak was medium-rear
+steak.mediumRear();
+//  action 3: the steak was well-done
+steak.wellDone();
 
 //  9)
+//  Noun: glass
+//  action 1: the glass was full
+glass.full();
+//  action 2: the glass was empty
+glass.empty();
+//  action 3: the glass was half full
+glass.halfFull();
+
+//  10) plane
 //  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
-
-//  10)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
+//  action 1: the plane was taking off
+plane.takeOff();
+//  action 2: the plane was landing
+plane.landing();
+//  action 3: the plane was taxiing
+plane.taxi();
 
 
 
