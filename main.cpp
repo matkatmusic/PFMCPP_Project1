4c6215a24dd98b9194a381a323322eafb71eb693
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-12-31 23:01:39 +0000

    fix: Add missing verb/action descriptions

--- main.cpp
+++ main.cpp
@@ -52,96 +52,97 @@ arm.rotateCounterClockwise();           // 4) demonstrates camelCase
 
 //  1)
  // Noun: fish
-//  action 1:
+//  action 1: The fish eats
 fish.eat();
-//  action 2:
+//  action 2: The fish swims 
 fish.swim();
-//  action 3:
+//  action 3: The fish breeds
 fish.breed();
 //  2)
 //  Noun: dog
-//  action 1:
+//  action 1: the dog barks
 dog.bark();
-//  action 2:
+//  action 2: the dog plays
 dog.play();
-//  action 3:
+//  action 3: the dog sit
 dog.sit();
 //  3)
 //  Noun: plane
-//  action 1:
+//  action 1: the ship departs
 ship.depart();
-//  action 2:
+//  action 2: the ship sails
 ship.sail();
-//  action 3:
-ship.dock();
+//  action 3: the ship docks
+ship.dock(); 
 //  4)
 //  Noun: car wash
-//  action 1:
+//  action 1: the carwash shampoos and waxes
 carWash.shampooAndWax();
-//  action 2:
+//  action 2: the carwash rinses
 carWash.rinse();
-//  action 3:
+//  action 3: the car wash blow dries
 carWash.blowDry();
 //  5)
 //  Noun: printer
-//  action 1:
+//  action 1: the printer prints
 printer.print();
-//  action 2:
+//  action 2: the printer scans
 printer.scan();
-//  action 3:
+//  action 3: the printer cleans itself
 printer.clean();
 //  6)
 //  Noun: engine
-//  action 1:
+//  action 1: the engine starts
 engine.start();
-//  action 2:
+//  action 2: the engine accelerates
 engine.accelerate();
-//  action 3:
+//  action 3: the engine stops
 engine.stop();
 //  7)
 //  Noun: tape recorder
-//  action 1:
+//  action 1: the tape recorder records
 tapeRecorder.record();
-//  action 2:
+//  action 2: the tape recorder rewinds
 tapeRecorder.rewind();
-//  action 3:
+//  action 3: the tape recorder plays
 tapeRecorder.play();
 //  8)
 //  Noun: coffee machine
-//  action 1:
+//  action 1: the coffee machine grinds beans
 coffeeMachine.grindBeans();
-//  action 2:
+//  action 2: the coffee machine heats water 
 coffeeMachine.heatWater();
-//  action 3:
+//  action 3: the coffee machine brews coffee
 coffeeMachine.brewCoffee();
 //  9)
 //  Noun: camera
-//  action 1:
+//  action 1: the camera focuses
 camera.focus();
-//  action 2:
+//  action 2: the camera zooms 
 camera.zoom();
-//  action 3:
+//  action 3: the camera captures and image
 camera.captureImage();
 //  10)
 //  Noun: elevator
-//  action 1:
+//  action 1: the elevator opens the door
 elevator.openDoor();
-//  action 2:
+//  action 2: the elevator closes the door
 elevator.closeDoor();
-//  action 3:
+//  action 3: the elevator ascends
 elevator.ascend();
-
 
 #endif
 
 /*
  MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
 
- Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
- 
- If you didn't already: 
+ Commit your changes by clicking on the Source Control panel on the left,
+ entering a message, and click [Commit and push].
+
+ If you didn't already:
     Make a pull request after you make your first commit
-    pin the pull request link and this repl.it link to our DM thread in a single message.
+    pin the pull request link and this repl.it link to our DM thread in a single
+ message.
 
  send me a DM to review your pull request when the project is ready for review.
 
@@ -149,8 +150,7 @@ elevator.ascend();
  */
 
 #include <iostream>
-int main()
-{
-    std::cout << "good to go" << std::endl;
-    return 0;
+int main() {
+  std::cout << "good to go" << std::endl;
+  return 0;
 }
